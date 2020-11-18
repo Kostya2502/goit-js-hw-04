c920059688f77a5906b8b6c29ca8153d17c35244
Kostya Panasiuk <kos2502@ukr.net>
2020-11-18 19:19:34 +0200

    ДЗ/4 посл

--- js/task_1.js
+++ js/task_1.js
@@ -1,27 +1,66 @@
 // Расставь отсутствующие this в методах объекта account.
 
-const account = {
-  owner: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
-  changeDiscount(value) {
-    this.discount = value;  
-  },
-  showOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost;
-    this.orders.push(order);
-  },
-};
+// const account = {
+//   owner: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['order-1', 'order-2', 'order-3'],
+//   changeDiscount(value) {
+//     this.discount = value;  
+//   },
+//   showOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost;
+//     this.orders.push(order);
+//   },
+// };
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+// account.changeDiscount(0.15);
+// console.log(account.discount); // 0.15
 
-console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+// console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
-console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+// account.addOrder(5000, 'order-4');
+// console.log(account.balance); // 19000
+// console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+
+
+
+
+
+// Функция mapArray(array, cb), принимает 1 - м параметром array - массив чисел,
+//   а вторым параметром cb - функцию обратного вызова(callback).Функция mapArray
+// создает новый массив numbers и заполняет его числами из массива array преобразованными функцией cb.
+// Оформи создание массива numbers нужной длины используя new Array()
+// и необходимый аргумент для задания длины, равной длине array.
+// Напиши функцию обратного вызова addIndex, которая принимает два параметра
+//   - element и index и возвращает число - сумму element и index(сложение).
+// Напиши функцию обратного вызова subIndex, которая принимает два параметра
+//   - element и index и возвращает число - разность element и index(вычитание).
+
+
+// const addIndex = (element, index) =>  element + index ;
+
+// const subIndex = (element, index) =>  element - index;
+  
+// function mapArray(array, cb) {
+//   'use strict';
+
+//   const numbers = new Array(array.length);
+//   for(let i = 0; i < array.length; i += 1) {
+//     const element = array[i];
+//     const index = i;
+//      numbers[i] = cb(element, index);
+//   }
+//   return numbers;
+// }
+
+// const arr  = [1,2,3,4,5];
+
+// console.log(mapArray(arr, addIndex));
+// // [1, 3, 5, 7, 9]
+
+// console.log(mapArray(arr, subIndex));
+// // [1, 1, 1, 1, 1]
--- js/task_2.js
+++ js/task_2.js
@@ -1,32 +1,62 @@
 // Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
 
-const inventory = {
-  items: ['Knife', 'Gas mask'],
-  add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
 
-    this.items.push(itemName);
-  },
-  remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
 
-    this.items = this.items.filter(item => item !== itemName);
-  },
-};
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
 
-const invokeInventoryAction = function(itemName, action) {
-  console.log(`Invoking action on ${itemName}`);
-  action(itemName);
-};
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
 
-invokeInventoryAction('Medkit', inventory.add.bind(inventory));
-// Invoking action on Medkit
-// Adding Medkit to inventory
+// invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
 
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
+// invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
 
-console.log(inventory.items); // ['Knife', 'Medkit']
+// console.log(inventory.items); // ['Knife', 'Medkit']
+
+
+
+
+
+// const isUniq = (element, index, arr) => arr.indexOf(element) === index;
+// const isEven = (element) => element % 2 === 0;
+
+// function filterArray(array, cb) {
+//   'use strict';
+//   const numbers = [];
+//   for(let i = 0; i < array.length; i += 1) {
+//     const element = array[i];
+//     const index = i;
+    
+//        if (cb(element, index, array)) {
+//       numbers.push(element);
+//     }
+    
+//   }
+//   return numbers;
+// }
+
+// const arr  = [1,2,3,4,5,1,2,5];
+
+// console.log(filterArray(arr, isUniq));
+// [1, 2, 3, 4, 5]
+
+// console.log(filterArray(arr, isEven));
+// [2, 4, 2]
